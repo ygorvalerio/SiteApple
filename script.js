7c41834a1c05941094c63270c744242e6a3e35f4
Ygor Galindo <ygorvalerio@gmail.com>
2024-09-25 00:19:17 -0300

    Alteração no layout principal.

---
+++ script.js
@@ -0,0 +1,10 @@
+function imgSlider(image) {
+    const imgElement = document.getElementById('phone'); 
+    imgElement.src = image; 
+}
+
+function circleChange(color) {
+    const circle = document.querySelector('.circle')
+    circle.style.background = color; 
+}
+
